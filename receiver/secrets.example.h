7ffe5bc627734a4064ba40027c3d84fcdf822746
Tomas Donko <tomas.donko@peercredit.com>
2025-11-01 19:19:28 +0100

    Added secrets example

---
+++ receiver/secrets.example.h
@@ -0,0 +1,7 @@
+#pragma once
+#define WIFI_SSID "wifiName" 
+#define WIFI_PASSWORD "wifiPassword"
+#define API_KEY "firebaseApiKey"
+#define USER_EMAIL "firebaseAllowedUserEmail"
+#define USER_PASSWORD "firebaseAllowedUserPassword"
+#define FIREBASE_PROJECT_ID "firebaseProjectId"
